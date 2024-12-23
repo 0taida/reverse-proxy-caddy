9bb43188490104c3fd5effa8c795f3cad9508a3c
0taida <mohemmed130@gmail.com>
2024-12-23 14:27:30 +0300

    first commit

---
+++ hello-store/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <h1>Hello store</h1>
+    <h3>Welcome to hello store</h3>
+  </div>
+</template>
+
---
+++ taida-store/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <h1>Taida store</h1>
+    <h3>Welcome to Taida store</h3>
+  </div>
+</template>
+
